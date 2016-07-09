6e05f0d495c643546acd8d9687e9a6bd6916cb6a
Leandro ROlim <superleandromail@gmail.com>
2016-07-09 15:19:08 -0300

    create a ServiceProvider for package

---
+++ src/Provider/AdminServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leand
+ * Date: 09/07/2016
+ * Time: 15:12
+ */
+
+namespace Lrcurso\Admin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+}
