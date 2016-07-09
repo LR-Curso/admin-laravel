3b0e3a36c0c192b366bfb9e57447102d7d45f8fb
Leandro ROlim <superleandromail@gmail.com>
2016-07-09 10:54:51 -0300

    first commit

---
+++ test/TextCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leandro Rolim
+ * Date: 09/07/2016
+ * Time: 10:41
+ */
+
+namespace Lrcurso\Tests;
+
+
+use \Orchestra\Testbench\TestCase;
+
+class TextCase extends TestCase
+{
+    
+}
