bd5c5fb168756eda3586cfecf2c20d3e598b6b43
Leandro ROlim <superleandromail@gmail.com>
2016-07-10 13:55:06 -0300

    add trait with methods for generate forms from arrays or model (Eloquent)

---
+++ src/Forms/FormGenerateTrait.php
@@ -0,0 +1,53 @@
+<?php
+namespace Lrcurso\Admin\Forms;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Kris\LaravelFormBuilder\Form;
+use Kris\LaravelFormBuilder\FormBuilderTrait;
+
+trait FormGenerateTrait
+{
+    use FormBuilderTrait;
+
+    /**
+     * @param array $form_fields
+     * @param string $form_action
+     * @param string $method
+     * @param Model|null $model
+     * @return Form
+     */
+    protected function getFormFromArray(
+        array $form_fields,
+        string $form_action = "",
+        string $method = "post",
+        Model $model = null
+    ): Form
+    {
+        $form = $this->plain([
+            'url' => $form_action,
+            'method' => 'post',
+            'model' => $model,
+        ]);
+
+        foreach($form_fields as $field => $values){
+            $form->add($field, $values['type'] ?? 'text', $values['options'] ?? []);
+        }
+        $form->add('Salvar', 'submit', ['attr' => ['class' => 'btn btn-primary']]);
+        return $form;
+    }
+
+
+    /**
+     * @param Model $model
+     * @return Form
+     */
+    protected function getFormFromModel(
+        Model $model,
+        string $form_action = "",
+        string $method = "post"
+    ): Form
+    {
+        return $this->getFormFromArray($model->getFillable(), $form_action, $method);
+    }
+}
