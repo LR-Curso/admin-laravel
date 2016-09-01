232ad08fd399e60b44e91307455127b5f16a1be0
Leandro Rolim <superleandromail@gmail.com>
2016-09-01 12:35:02 -0300

    generate crud for model

--- src/Controllers/AdminCrud.php
+++ src/Controllers/AdminCrud.php
@@ -19,6 +19,10 @@ trait AdminCrud
     abstract protected function getModel(): Model;
 
     /**
+     * @var Request
+     */
+    protected $request = null;
+    /**
      * @return string
      */
     public abstract function getTitle():string;
@@ -41,28 +45,52 @@ trait AdminCrud
     public function create()
     {
         return view('lrcurso_admin::admin.crud.form', [
-            'form' => $this->getFormFromModel($this->getModel()),
+            'form' => $this->getFormFromModel($this->getModel(), action("\\".self::class."@store")),
             'title' => $this->getTitle(),
         ]);
     }
 
     public function store()
     {
-
+        $this
+            ->getModel()
+            ->fill($this->request()->all())
+            ->save();
+        redirect()->action("\\".self::class."@index");
     }
 
+    /**
+     * @param $id
+     * @return View
+     */
     public function edit($id)
     {
-
+        $model = $this->getModel()->newQuery()->where('id', $id)->firstOrFail();
+        return view('lrcurso_admin::admin.crud.form', [
+            'form' => $this->getFormFromModel($model, action("\\".self::class."@update", [$id]), "PUT"),
+            'title' => $this->getTitle(),
+        ]);
     }
 
-    public function update($id, Request $request)
+    public function update($id)
     {
-
+        $model = $this->getModel()->newQuery()->where('id', $id)->firstOrFail();
+        $model->fill($this->request()->all())->save();
+        return redirect()->action("\\".self::class."@index");
     }
 
     public function destroy($id)
     {
-        
+        $model = $this->getModel()->newQuery()->where('id', $id)->firstOrFail();
+        $model->delete();
+        return redirect()->action("\\".self::class."@index");
+    }
+
+    /**
+     * @return Request
+     */
+    protected function request()
+    {
+        return $this->request ?? request();
     }
 }
